ff9f764cee54c5845b10a7a49ecffb689b2bca54
Rachit Mittal <rachit18302@iiitd.ac.in>
2019-07-18 03:37:56 +0530

    End of Game- need to add a label

--- app.py
+++ app.py
@@ -47,25 +47,36 @@ for i in range(9):
 	canvas.create_rectangle(xcoord[i],ycoord[i],xcoord[i]+200,ycoord[i]+200,fill="white")
 canvas.pack(side=TOP)
 #contruction ended here
-
+win = False
 
 ser = serial.Serial("/dev/cu.usbmodem1411",9600) #data from serial monitor
 places = [[0]*9,[0]*9] # Array for places 9 by 9 
 colourScheme = ["blue","red"] #colour scheme for notifying player 1 and 2 respectively
 
 if start:
-	while (True):
-		newData = str(ser.readline())
-		newData = newData[2:-5]
-		data = list(map(int,newData.split()))
-		try:
-			for i in range(9):
-				if (places[player][i]==0 and places[player^1][i]==0):
-					places[player][i] = data[i]
-		except IndexError:
-			pass
-		for i in range(9):
-			if places[player][i] == 1:
-				canvas.create_rectangle(xcoord[i],ycoord[i],xcoord[i]+200,ycoord[i]+200,fill=colourScheme[player])
+    while (True):
+        if not win:
+            newData = str(ser.readline())
+            newData = newData[2:-5]
+            data = list(map(int,newData.split()))
+            try:
+                for i in range(9):
+                    if (places[player][i]==0 and places[player^1][i]==0):
+                        places[player][i] = data[i]
+            except IndexError:
+                pass
+            for i in range(9):
+                if places[player][i] == 1:
+                    canvas.create_rectangle(xcoord[i],ycoord[i],xcoord[i]+200,ycoord[i]+200,fill=colourScheme[player])
+            
+            if(places[player][4]==1):
+                if((places[player][0]==1 and places[player][8]==1) or (places[player][1]==1 and places[player][7]==1) or (places[player][2]==1 and places[player][6]==1) or (places[player][3]==1 and places[player][5]==1)):
+                    win = True
+          
+            elif((places[player][0]==1 and places[player][3]==1 and places[player][6]==1)or(places[player][0]==1 and places[player][1]==1 and places[player][2]==1)or(places[player][6]==1 and places[player][7]==1 and places[player][8]==1)or(places[player][2]==1 and places[player][5]==1 and places[player][8]==1)):
+                win = True
+        else:
+            canvas.create_rectangle(0,0,640,640,fill="black")
+
 
 root.mainloop()
